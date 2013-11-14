0d5e260659e3d2be4a163bd0dc9534559789bf06
HunJae Lee <hunjae.lee@gmail.com>
2013-11-14 12:27:03 +0900

    Add sample code

--- src/java/TitanTutorial.java
+++ src/java/TitanTutorial.java
@@ -21,7 +21,8 @@ public class TitanTutorial {
 		//getCount(g, time - 2000, "Request");
 		//getUniqueCount(g, time + 2000, "Request", "clientIp");
 		//getUniqueCount(g, time - 2000, "Request", "clientIp");
-		getWeavingPoints(g, time - 5000, "Response");
+		//getWeavingPoints(g, 1384326237000L, "Response");
+		getResponseTimeDetail(g, 508362204L);
 
 		g.shutdown();
 	}
@@ -35,6 +36,76 @@ public class TitanTutorial {
 
 	}
 
+	public static void getResponseTimeDetail(TitanGraph g, long vertexId) {
+		Vertex v = g.getVertex(vertexId);
+		if(v == null) {
+			//TODO
+			return;
+		} else {
+			System.out.println("found vertex by id");
+		}
+
+		String uuid = v.getProperty("uuid");
+		if(uuid == null) {
+			//TODO
+			return;
+		} else {
+			System.out.println("found uuid: " + uuid);
+		}
+
+		Iterator<Vertex> vIt = g.getVertices("guuid", uuid).iterator();
+
+		String clientIp = null;
+		String url = null;
+		int responseTime = 0;
+		String entryOperationName = null;
+		int entryOperationElapsedTime = 0;
+		int controllerElapsedTime = 0;
+		int daoElapsedTime = 0;
+		int sqlElapsedTime = 0;
+		
+		String event = null;
+		if(vIt.hasNext()) {
+			Vertex uuidV = vIt.next();
+			for(Vertex vertex: uuidV.query().labels("flow").vertices()) {
+				event = vertex.getProperty("event");
+				System.out.println("Event: " + event);
+				if("Request".equals(event)) {
+					url = vertex.getProperty("url");
+					clientIp = vertex.getProperty("clientIp");
+				}
+				if("Response".equals(event)) {
+					responseTime = vertex.getProperty("elapsedTime");
+				}
+				if("EntryOperationReturn".equals(event)) {
+					entryOperationElapsedTime = vertex.getProperty("elapsedTime");
+				}
+				if("OperationReturn".equals(event)) {
+					int depth = vertex.getProperty("depth");
+					int elapsedTime = vertex.getProperty("elapsedTime");
+					if(depth  == 2) {
+						controllerElapsedTime += elapsedTime;
+					} else if(depth == 3) {
+						daoElapsedTime += elapsedTime;
+					} else {
+						System.out.println("unknown depth = " + depth);
+					}
+				}
+				if("SqlReturn".equals(event)) {
+					int elapsedTime = vertex.getProperty("elapsedTime");
+					sqlElapsedTime += elapsedTime;
+				}
+			}
+		}
+
+		System.out.println("Client IP: " + clientIp);
+		System.out.println("URL: " + url);
+		System.out.println("ResponseTime: " + responseTime);
+		System.out.println("EntryOpTime: " + entryOperationElapsedTime);
+		System.out.println("controllerElapsedTime: " + controllerElapsedTime);
+		System.out.println("daoTime: " + daoElapsedTime);
+		System.out.println("sqlTime: " + sqlElapsedTime);
+	}
 	public static void getWeavingPoints(TitanGraph g, long secondInMillis,  String target) {
 		long second =  (secondInMillis / 1000) * 1000;
 
