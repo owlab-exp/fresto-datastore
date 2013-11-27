a3305b4d071820d2cbb62c87c25d3d1fe26fd20a
HunJae Lee <hunjae.lee@gmail.com>
2013-11-27 14:47:28 +0900

    Examplify startup script

--- src/java/TitanInitialization.java
+++ src/java/TitanInitialization.java
@@ -11,7 +11,15 @@ import com.tinkerpop.blueprints.Vertex;
 import com.tinkerpop.blueprints.Direction;
 
 public class TitanInitialization {
+	private static String CASSANDRA_HOST;
+
 	public static void main(String[] args) {
+		if(args.length < 1) {
+			System.out.println("An argument needed: <Host name of Cassandra>");
+			System.exit(1);
+		}
+		CASSANDRA_HOST = args[0];
+
 		initialize();
 	}
 
@@ -19,27 +27,15 @@ public class TitanInitialization {
 		Configuration conf = new BaseConfiguration();
 		//Cassandra Configuration
 		conf.setProperty("storage.backend", "cassandra");
-		conf.setProperty("storage.hostname", "fresto2.owlab.com");
+		conf.setProperty("storage.hostname", CASSANDRA_HOST);
 
 		//Elasticsearch Configuration
-		conf.setProperty("storage.index.search.backend", "elasticsearch");
-		conf.setProperty("storage.index.search.hostname", "fresto2,fresto3,fresto4");
-		//conf.setProperty("storage.index.search.index-name", "fresto");
-		conf.setProperty("storage.index.search.client-only", "true");
 		TitanGraph g = TitanFactory.open(conf);
 
 		/**
-		//// To search event by its name
-		////g.makeType().name("event").dataType(String.class).indexed(Vertex.class).unique(Direction.OUT).indexed(Edge.class).unique(Direction.OUT).makePropertyKey();
-		////g.makeType().name("uuid").dataType(String.class).indexed(Vertex.class).unique(Direction.OUT).makePropertyKey();
-		////g.makeType().name("sequence").dataType(Integer.class).indexed(Vertex.class).unique(Direction.OUT).makePropertyKey();
-		////g.makeType().name("timestamp").dataType(Long.class).indexed(Vertex.class).unique(Direction.OUT).makePropertyKey();
-		////g.makeType().name("elapsedTime").dataType(Long.class).indexed(Vertex.class).unique(Direction.OUT).makePropertyKey();
-
-		////g.makeType().name("second").dataType(Long.class).indexed(Vertex.class).makePropertyKey();
 		*/
-		//g.makeType().name("second").dataType(Long.class).indexed(Vertex.class).unique(Direction.OUT).makePropertyKey();
-		//g.makeType().name("guuid").dataType(String.class).indexed(Vertex.class).unique(Direction.OUT).makePropertyKey();
+		g.makeType().name("second").dataType(Long.class).indexed(Vertex.class).unique(Direction.OUT).makePropertyKey();
+		g.makeType().name("guuid").dataType(String.class).indexed(Vertex.class).unique(Direction.OUT).makePropertyKey();
 
 		
 		g.commit();
